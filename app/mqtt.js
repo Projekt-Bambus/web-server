b2345d6f0a5a6da2f9e74c9f221f19686fc47cd5
Pavel Dobiáš <pavel23dob@gmail.com>
2024-06-11 00:54:35 +0200

    Add files via upload

--- app/mqtt.js
+++ app/mqtt.js
@@ -1,37 +1,33 @@
-const Mqtt = require("mqtt");
-const Server = require("./server.js")
+const mqtt = require("mqtt");
 const MQTT_BROKER_ADDRESS = "mqtt://trejbycloud.duckdns.org:31011";
 const TOPIC_SEND = "poslouchat";
 const TOPIC_RECIEVE = "posilat";
 
-const mqttClient = Mqtt.connect(MQTT_BROKER_ADDRESS);
-
-function processMessage(prop, value) {
-    
-}
+const mqttClient = mqtt.connect(MQTT_BROKER_ADDRESS);
 
 function sendMessage(prop, value) {
-    mqttClient.publish(TOPIC_SEND, `${prop}:${value}`);
+	console.log(`Publish MQTT message [${TOPIC_SEND}]: '${prop}:${value}'`);
+	mqttClient.publish(TOPIC_SEND, `${prop}:${value}`);
 }
 
-function main() {
-    mqttClient.on("connect", () => {
-        mqttClient.subscribe(TOPIC_SEND, (err) => {
-            if (!err) {
-                mqttClient.publish(TOPIC_SEND, "Projekt Bambus!");
-            }
-        });
-    });
-    
-    mqttClient.on(TOPIC_RECIEVE, (topic, message) => {
-        const msgText = message.toString();
-        const [prop, value] = msgText.split(":");
-        processMessage(prop,parseInt(value));
-        mqttClient.end();
-    
-        console.log(`MSG: '${message.toString()}' TOPIC: '${topic}'`);
-    });
+async function main() {
+	mqttClient.on("connect", () => {
+		mqttClient.subscribe(TOPIC_SEND, (err) => {
+			if (!err) {
+				mqttClient.publish(TOPIC_SEND, "Projekt Bambus!");
+			}
+		});
+	});
+		
+	mqttClient.on(TOPIC_RECIEVE, (topic, message) => {
+		console.log(`Recieve MQTT message [${topic}]: '${message.toString()}'`);
+		const msgText = message.toString();
+		const [key, value] = msgText.split(":");
+		processMessage(key,parseInt(value));
+		mqttClient.end();
+		
+		console.log(`MSG: '${message.toString()}' TOPIC: '${topic}'`);
+	});
 }
 
 module.exports = { main, sendMessage }
-
--- app/www/login/script.js
+++ app/www/login/script.js
@@ -1,36 +1,57 @@
 const USERNAME_VALUE_ID = "username-input";
 const PASSWORD_VALUE_ID = "password-input";
 
+// Constant URLs
 const ROOT_URL = location.protocol + '//' + location.host;
 const LOGIN_URL = ROOT_URL + "/login";
 
+let loginInProgress = false;
+
+//# Login fail feedback
 function wrongLoginFeedback() {
-    const errorImage = document.getElementById("wrong");
-    errorImage.style.display = "block";
-    const errorSound = document.getElementById("wrong-sound");
-    errorSound.play();
-    setTimeout(function() {
-        errorImage.style.display = "none";
-    }, 1000);
+	const errorImage = document.getElementById("wrong");
+	errorImage.style.display = "block";
+	const errorSound = document.getElementById("wrong-sound");
+	errorSound.play();
+	setTimeout(function() {
+		errorImage.style.display = "none";
+	}, 1000);
 }
 
-function submitLogin() {
-    fetch(LOGIN_URL, {
-        method: "POST",
-        body: JSON.stringify({
-          username: document.getElementById(USERNAME_VALUE_ID).value,
-          password: document.getElementById(PASSWORD_VALUE_ID).value,
-        }),
-        headers: {
-          "Content-type": "application/json"
-        }
-    }).then((res) => {
-      if (res.status == 200) {
-        window.location.replace(ROOT_URL);
-      } else if (res.status == 401) {
-          wrongLoginFeedback();
-      } else {
-        alert(`[${res.status}] - ${res.body}`);
-      }
-    })
+//# Login request
+async function submitLogin() {
+	// Prevent sending multiple requests at once
+	if (loginInProgress) {
+		return;
+	} else {
+		loginInProgress = true;
+	}
+	// Login post request
+	await fetch(LOGIN_URL, {
+		method: "POST",
+		body: JSON.stringify({
+			username: document.getElementById(USERNAME_VALUE_ID).value,
+			password: document.getElementById(PASSWORD_VALUE_ID).value,
+		}),
+		headers: {
+			"Content-type": "application/json"
+		}
+	}).then((res) => {
+		if (res.status == 200) {
+			window.location.replace(ROOT_URL);
+		} else if (res.status == 401) {
+			wrongLoginFeedback();
+		} else {
+			alert(`[${res.status}] - ${res.body}`);
+		}
+	});
+	// Allow potential new request
+	loginInProgress = false;
 }
+
+//# Login on Enter
+document.addEventListener('keyup', (e) => {
+  if (e.key == "Enter") {
+    submitLogin();
+  }
+})
